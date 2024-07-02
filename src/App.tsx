c41069cfb96377e6fdb4e43be131a3291c8daed7
gerson aguiar <gersoncafilho@gmail.com>
2024-07-01 23:59:16 -0300

    v0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,84 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { getDatabase, ref, set } from 'firebase/database';
+import { Box } from '@mui/material';
+
+const generateShortUrl = (): string => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let shortUrlKey = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    shortUrlKey += characters[randomIndex];
+  }
+  return shortUrlKey;
+  // return `https://shortlink-86962.web.app/${shortUrl}`;
+};
+
 
 function App() {
+
   const [count, setCount] = useState(0)
+
+  const [url, setUrl] = useState("");
+
+
+  
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+
+    const db = getDatabase();
+
+    const eventRef = ref(db, 'events/' + Date.now());
+    set(eventRef, {
+      t: "title",
+    })      .then(() => {
+        alert('Evento cadastrado com sucesso!');
+            })
+      .catch(() => {
+        alert('Erro ao criar URL');
+      });
+    
+
+
+    if (url) {
+      const shortUrlKey = generateShortUrl();
+      const shortUrl = `https://short.ly/${shortUrlKey}`;
+
+      try {
+        await set(ref(db, 'shortlinks/' + shortUrlKey), {
+          originalUrl: url,
+          shortUrl: shortUrl
+        });
+        setUrl("");
+        alert("URL encurtada com sucesso!");
+      } catch (error) {
+        console.error("Erro ao encurtar URL: ", error);
+      }
+    }
+  };
+
 
   return (
     <>
+
+    <form onSubmit={handleSubmit}>
+      <Box display="flex" flexDirection="column">
+        <input
+          type="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Insira a URL"
+          required
+          />
+
+            <button type="submit">Encurtar URL</button>
+      </Box>
+    </form>
+{/* 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,8 +86,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,9 +95,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </div> */}
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Digite a URL que deseja encurtar
       </p>
     </>
   )
---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2CtpWyZekfMS6vxvgXJwafCe3C-CzP9E",
+  authDomain: "shortlink-86962.firebaseapp.com",
+  databaseURL: "https://shortlink-86962-default-rtdb.firebaseio.com",
+  projectId: "shortlink-86962",
+  storageBucket: "shortlink-86962.appspot.com",
+  messagingSenderId: "951033020316",
+  appId: "1:951033020316:web:c8ce13b21dc5f5ee4cd259",
+  measurementId: "G-N63XMXZ3YJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
